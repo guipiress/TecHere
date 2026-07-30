ff2b49a2cbb69fd68fc5fc7f01357b1b905c0aec
Guilherme Pires <guisantospires.238@gmail.com>
2026-07-30 15:04:12 -0300

    Create address table

--- database/schema.sql
+++ database/schema.sql
@@ -12,3 +12,19 @@ cpf CHAR(11) UNIQUE NOT NULL
 );
 
 
+--===============================
+--ADDRESS
+CREATE TABLE address (
+    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES users(id),
+    street VARCHAR(100) NOT NULL,
+    street_number VARCHAR(10) NOT NULL,
+    district VARCHAR(100),
+    city VARCHAR(100) NOT NULL,
+    state VARCHAR(100),
+    zip_code VARCHAR(20) NOT NULL,
+    country CHAR(2) NOT NULL,
+    complement VARCHAR(100)
+);
+
+
