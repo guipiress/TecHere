ba45e75fa8cf7d8cf5f7786085d68eb7995c21c0
Guilherme Pires <guisantospires.238@gmail.com>
2026-08-03 17:06:50 -0300

    Add orders validation constraints

--- database/schema.sql
+++ database/schema.sql
@@ -5,7 +5,7 @@ CREATE TABLE users (
 id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 name VARCHAR(50) NOT NULL,
 role VARCHAR(20) NOT NULL
- CHECK (role IN ('ADMIN', 'EMPLOYEE', 'CUSTOMER')      --roles: ADMIN, EMPLOYEE, CUSTOMER
+CHECK (role IN ('ADMIN', 'EMPLOYEE', 'CUSTOMER')      --roles: ADMIN, EMPLOYEE, CUSTOMER
 ),
 email VARCHAR(255) UNIQUE NOT NULL,
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -19,16 +19,16 @@ cpf CHAR(11) UNIQUE NOT NULL
 --ADDRESS
 --===============================
 CREATE TABLE address (
-    id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-    user_id INTEGER NOT NULL REFERENCES users(id),
-    street VARCHAR(100) NOT NULL,
-    street_number VARCHAR(10) NOT NULL,
-    district VARCHAR(100) NOT NULL,
-    city VARCHAR(100) NOT NULL,
-    state VARCHAR(100) NOT NULL,
-    zip_code VARCHAR(20) NOT NULL,
-    country CHAR(2) NOT NULL,
-    complement VARCHAR(100)
+id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+user_id INTEGER NOT NULL REFERENCES users(id),
+street VARCHAR(100) NOT NULL,
+street_number VARCHAR(10) NOT NULL,
+district VARCHAR(100) NOT NULL,
+city VARCHAR(100) NOT NULL,
+state VARCHAR(100) NOT NULL,
+zip_code VARCHAR(20) NOT NULL,
+country CHAR(2) NOT NULL,
+complement VARCHAR(100)
 );
 
 
@@ -62,12 +62,14 @@ description TEXT
 --===============================
 --ORDERS
 --===============================
-CREATE TABLE order (
+CREATE TABLE orders (
 id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
 user_id INTEGER NOT NULL REFERENCES users(id),
 address_id INTEGER NOT NULL REFERENCES address(id),
-status VARCHAR(50) NOT NULL,
-total_price NUMERIC(10,2) NOT NULL,
+status VARCHAR(20) NOT NULL
+    CHECK (status IN ('PENDING', 'PAID', 'CANCELLED')),
+total_price NUMERIC(10,2) NOT NULL
+    CHECK (total_price >= 0),
 created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
