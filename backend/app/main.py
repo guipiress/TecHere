609ed16b6e4552bbcf7e4615c00fe640ee8cc850
Guilherme Pires <guisantospires.238@gmail.com>
2026-09-21 15:31:48 -0300

    Create product.py file

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,43 +3,6 @@ from fastapi import FastAPI, HTTPException, status
 from pydantic import BaseModel
 from decimal import Decimal
 
-class Product(BaseModel):
-    name: str
-    purchase_price: Decimal
-    sale_price: Decimal
-    stock: int
-    brand: str
-    category_id: int
-    description: str
-
-class ProductCreate(BaseModel):
-    name: str
-    purchase_price: Decimal 
-    sale_price: Decimal 
-    stock: int
-    brand: str
-    category_id: int
-    description: str | None = None
-
-class ProductUpdate(BaseModel):
-    name: str
-    purchase_price: Decimal
-    sale_price: Decimal
-    stock: int
-    brand: str
-    category_id: int
-    description: str | None = None
-
-class ProductResponse(BaseModel):
-    id : int
-    name: str
-    purchase_price: Decimal
-    sale_price: Decimal
-    stock: int
-    brand: str
-    category_id: int
-    description: str | None = None
-
     
 app = FastAPI()
 
